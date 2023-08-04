0e011642d5080d82c2afa1ee8d7042e6bfc7e0d2
unknown <daniel.hurtadoramirez314@gmail.com>
2023-08-04 16:27:45 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { default as Shaku } from "shaku"
+await Shaku.init();
+document.body.appendChild(Shaku.gfx.canvas);
+Shaku.gfx.setResolution(800, 600, true);
+function step() {
+	Shaku.startFrame();
+	Shaku.gfx.clear(Shaku.utils.Color.cornflowerblue);
+	Shaku.endFrame();
+	Shaku.requestAnimationFrame(step);
+}
+step();
