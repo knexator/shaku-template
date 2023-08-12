7c59f1ee73d6cf836c2f9fc19effbfddfff4d205
unknown <daniel.hurtadoramirez314@gmail.com>
2023-08-12 11:43:30 +0200

    typings

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import GUI from "lil-gui"
 
 // TODO: fix Shaku so these are proper imports
 const Vector2 = Shaku.utils.Vector2;
-const KeyboardKeys = Shaku.input.KeyboardKeys;
+const KeyboardKeys = Shaku.input!.KeyboardKeys;
 
 // the game's assets
 import example_texture_url from "./images/sprite.png?url"
@@ -17,18 +17,19 @@ gui.add(CONFIG, "move_speed", 10, 500);
 
 await Shaku.init();
 
+
 // load texture (TODO: change the example to showcase parallel loading)
 let example_texture = await Shaku.assets.loadTexture(example_texture_url);
 
 // use Shaku's canvas
-document.body.appendChild(Shaku.gfx.canvas);
-Shaku.gfx.setResolution(800, 600, true);
+document.body.appendChild(Shaku.gfx!.canvas);
+Shaku.gfx!.setResolution(800, 600, true);
 
 // create a SpriteBatch, which lets us draw sprites
-let example_batch = new Shaku.gfx.SpriteBatch();
+let example_batch = new Shaku.gfx!.SpriteBatch();
 
 // actual game state
-let player_position = new Vector2(Shaku.gfx.canvas.width / 2, Shaku.gfx.canvas.height / 2);
+let player_position = new Vector2(Shaku.gfx!.canvas.width / 2, Shaku.gfx!.canvas.height / 2);
 
 // main loop
 function step() {
@@ -37,24 +38,24 @@ function step() {
 	// LOGIC
 	let move_dir = new Vector2(0, 0);
 	// TODO: showcase a more concise way of doing this
-	if (Shaku.input.down([KeyboardKeys.W, KeyboardKeys.UP])) {
+	if (Shaku.input!.down([KeyboardKeys.W, KeyboardKeys.UP])) {
 		console.log("up")
 		move_dir.y -= 1;
 	}
-	if (Shaku.input.down([KeyboardKeys.S, KeyboardKeys.DOWN])) {
+	if (Shaku.input!.down([KeyboardKeys.S, KeyboardKeys.DOWN])) {
 		move_dir.y += 1;
 	}
-	if (Shaku.input.down([KeyboardKeys.D, KeyboardKeys.RIGHT])) {
+	if (Shaku.input!.down([KeyboardKeys.D, KeyboardKeys.RIGHT])) {
 		move_dir.x += 1;
 	}
-	if (Shaku.input.down([KeyboardKeys.A, KeyboardKeys.LEFT])) {
+	if (Shaku.input!.down([KeyboardKeys.A, KeyboardKeys.LEFT])) {
 		move_dir.x -= 1;
 	}
 	player_position.addSelf(move_dir.mul(CONFIG.move_speed * Shaku.gameTime.delta));
 
 	// VISUALS
 	// clear background
-	Shaku.gfx.clear(Shaku.utils.Color.cornflowerblue);
+	Shaku.gfx!.clear(Shaku.utils.Color.cornflowerblue);
 	// draw a sprite
 	example_batch.begin();
 	example_batch.drawQuad(example_texture, player_position, new Vector2(200, 200));
